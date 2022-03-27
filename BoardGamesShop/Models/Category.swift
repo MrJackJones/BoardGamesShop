ce0f14a247d1becdafa4ec979f4e797a627015ad
back2 <back2@local.ru>
2022-03-27 22:36:40 +0300

    add Models and DataManager

--- BoardGamesShop/Models/Category.swift
+++ BoardGamesShop/Models/Category.swift
@@ -6,3 +6,7 @@
 //
 
 import Foundation
+
+struct Category {
+    let name: String
+}
--- BoardGamesShop/Models/Product.swift
+++ BoardGamesShop/Models/Product.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+
+struct Product {
+    let name: String
+    let tagline: String
+    let description: String
+    let image: String
+    let price: Float
+    let manufacturer: Manufacturer
+    let equipmens: [Equipment]
+    let feedbacks: [Feedback]
+    let category: Category
+    let playerAge: Int
+    let roundTime: Int
+    let playerCountMin: Int
+    let playerCountMax: Int
+    let releaseYear: Int
+}
+
+
+struct Equipment {
+    let name: String
+}
+
+struct Feedback {
+    let authorName: String
+    let text: String
+}
+
+enum Category: String {
+    case party = "Вечериночные игры"
+    case qizzes = "Викторины"
+    case detective = "Детективные игры"
+    case children = "Детские игры"
+    case duel = "Дуэльные игры"
+}
+
+enum Manufacturer: String {
+    case hobbyWorld = "Hobby World"
+    case economicus = "Экономикус"
+    case zvezda = "Звезда"
+    case magellan = "Magellan"
+    case gaga = "GaGa Games"
+    case spinMaster = "Spin Master"
+}
