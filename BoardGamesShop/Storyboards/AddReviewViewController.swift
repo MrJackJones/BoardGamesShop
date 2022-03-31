9d7ce68f28668ddd1a3c7b0686fc09cb399a042c
anastasiiakorhsina <anastasiiakorshina@gmail.com>
2022-04-01 00:02:15 +0300

    1) Setting storyboard 2) Create new viewController files

---
+++ BoardGamesShop/Storyboards/AddReviewViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddReviewViewController.swift
+//  BoardGamesShop
+//
+//  Created by Anastasiia Korshina on 31.03.2022.
+//
+
+import UIKit
+
+class AddReviewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
